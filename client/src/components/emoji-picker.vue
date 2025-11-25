0bcfed67fb1667b0f3b11cc2e8e8a9b9fbab3e39
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-11-26 00:32:23 +0800

    remove: node modules in root dir

--- client/src/components/emoji-picker.vue
+++ client/src/components/emoji-picker.vue
@@ -2,9 +2,15 @@
   <Transition>
     <div
       v-if="showEmoji"
-      class="absolute bottom-full right-30 z-10 mb-2 bg-gray-100 rounded-xs border border-gray-200 p-4"
+      class="absolute bottom-full right-30 z-10 mb-2 bg-gray-100 rounded-xs border border-gray-200 p-4 w-[200px]"
     >
-      <img src="/assets/emojis/bento-box.gif" alt="" class="emoji" />
+      <div class="flex flex-row gap-5">
+        <div v-for="emoji in availableEmojis">
+          <button type="button" @click="clickHandler(emoji)">
+            <img :src="emoji.path" alt="" class="emoji" />
+          </button>
+        </div>
+      </div>
     </div>
   </Transition>
 </template>
@@ -21,12 +27,16 @@
   opacity: 0;
   scale: 0;
 }
+
 .emoji:hover {
   scale: 1.2;
 }
+
 .emoji {
+  width: 30px;
   cursor: pointer;
   transition: all 0.3s ease;
+  object-fit: cover;
 }
 </style>
 
@@ -36,6 +46,22 @@ export default {
     showEmoji: {
       type: Boolean,
       required: true,
+    },
+  },
+
+  data() {
+    return {
+      availableEmojis: [
+        { path: "/assets/emojis/heart_face.png", emoji: "😍" },
+        { path: "/assets/emojis/crying_face.png", emoji: "😭" },
+      ],
+    };
+  },
+
+  methods: {
+    clickHandler({ emoji }) {
+      //   console.log(emoji);
+      this.$emit("emoji-selected", emoji);
     },
   },
 };
