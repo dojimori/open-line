e8e6a41fda83bd255e336cd8ec813445461e11c6
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-16 09:53:32 +0800

    profile link

--- client/src/components/header-component.vue
+++ client/src/components/header-component.vue
@@ -1,30 +1,25 @@
 <template>
   <!-- header -->
-  <div
-    class="header w-full py-2.5 px-4 flex md:flex-row flex-col justify-between items-center"
-  >
+  <div class="header w-full py-2.5 px-4 flex md:flex-row flex-col justify-between items-center">
     <h1 class="text-white font-bold">> Open Line</h1>
 
     <div>
-      <router-link
-        to="/chat"
-        class="header-btn hover:translate-y-[-1.5px] cursor-pointer"
-      >
+      <router-link to="/chat" class="header-btn hover:translate-y-[-1.5px] cursor-pointer">
         <ph-globe></ph-globe>
         open line
       </router-link>
-      <router-link
-        to="/profile/edit"
-        class="header-btn hover:translate-y-[-1.5px] cursor-pointer"
-      >
+
+      <router-link to="/chat" class="header-btn hover:translate-y-[-1.5px] cursor-pointer">
+        <ph-user></ph-user>
+        profile
+      </router-link>
+
+      <router-link to="/profile/edit" class="header-btn hover:translate-y-[-1.5px] cursor-pointer">
         <ph-pencil-simple></ph-pencil-simple>
         edit profile
       </router-link>
 
-      <button
-        class="header-btn hover:translate-y-[-1.5px] cursor-pointer"
-        @click="logout"
-      >
+      <button class="header-btn hover:translate-y-[-1.5px] cursor-pointer" @click="logout">
         <ph-sign-out></ph-sign-out>
         Logout
       </button>
@@ -33,7 +28,7 @@
 </template>
 
 <script>
-import { PhSignOut, PhPencilSimple, PhGlobe } from "@phosphor-icons/vue";
+import { PhSignOut, PhPencilSimple, PhGlobe, PhUser } from "@phosphor-icons/vue";
 import authApi from "@/utils/api/auth.api";
 import { socket } from "@/utils/socket";
 import { useStore } from "@/store";
@@ -43,6 +38,7 @@ export default {
     PhPencilSimple,
     PhSignOut,
     PhGlobe,
+    PhUser,
   },
 
   // props: {
