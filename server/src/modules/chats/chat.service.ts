cda29cb37db0249d9ac82d619c59768d590ccd4e
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2026-01-14 21:42:22 +0800

    refactor: use chatservice create in chat handler

--- server/src/modules/chats/chat.service.ts
+++ server/src/modules/chats/chat.service.ts
@@ -1,12 +1,16 @@
 import chatRepository from "./chat.repository";
+import { Chat } from "../../../generated/prisma/client";
 
 class ChatService {
-  async getAll() {
+  async create(message: string, type: string, userId: number): Promise<Chat> {
+    return chatRepository.create(message, type, userId);
+  }
+
+  async getAll(): Promise<Chat[]| []> {
     const chats = await chatRepository.getAll();
     chats?.reverse();
     return chats;
   }
-
 }
 
 export default new ChatService();
--- server/src/sockets/handlers/chat.handler.ts
+++ server/src/sockets/handlers/chat.handler.ts
@@ -2,6 +2,8 @@ import { Server, Socket } from "socket.io"
 import { users } from "..";
 import { prisma } from "../../../lib/prisma";
 import userService from "../../modules/users/user.service";
+import chatService from "../../modules/chats/chat.service";
+
 
 export const chatHandler = (io: Server, socket: Socket) => {
     socket.on('chat:message', async (data) => {
@@ -19,15 +21,16 @@ export const chatHandler = (io: Server, socket: Socket) => {
             return;
         }
 
-        await prisma.chat.create({
-            data: {
-                message,
-                type: 'chat',
-                user: {
-                    connect: { id: user.id }
-                }
-            }
-        })
+        await chatService.create(message, 'chat', user.id);
+        // await prisma.chat.create({
+        //     data: {
+        //         message,
+        //         type: 'chat',
+        //         user: {
+        //             connect: { id: user.id }
+        //         }
+        //     }
+        // })
 
     });
 }
